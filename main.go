590b7147ce8094857a0f549b909eb517a3a8007b
Jason Steinberg <jsteinberg555@gmail.com>
2021-01-21 18:38:10 -0700

    Do some more setup

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"library/database"
 	"library/server"
 )
 
 func main() {
 	fmt.Println("Welcome to the Library...")
+	database.LoadDatabaseConfig("")
 	server.SetupRouter()
 }
