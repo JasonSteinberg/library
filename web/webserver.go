a228d50321dcfdf71269b9a82ac7f8e5672e5027
Jason Steinberg <jsteinberg555@gmail.com>
2021-01-23 08:08:04 -0700

    favicon

--- web/webserver.go
+++ web/webserver.go
@@ -12,6 +12,7 @@ var (
 func SetUpWebRoutes(router *gin.Engine, routePath string) {
 	router.LoadHTMLGlob("templates/*")
 	router.StaticFile("/styles.css", "./resources/styles.css")
+	router.StaticFile("/favicon.ico", "./resources/favicon.ico")
 	router.GET("/index.html", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
 			"title":    "The Library",
